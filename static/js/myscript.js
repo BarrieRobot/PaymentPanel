dabe0b801e07ed118e163709be549b8b4e34d3be
Casper Desktop <casper.cromjongh@hotmail.com>
2017-12-09 11:55:11 +0100

    Improved layout, added table for all products. One should be able to change the price of products by clicking on the price. It is saved by clicking outside.

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -4,9 +4,33 @@ var vm = new Vue({
   delimiters: ['[[', ']]'],
   data: {
     tag_id: "Geen NFC tag",
-    orders:  ["None"],
-    loadingtext: ''
+    orders:  [],
+    loadingtext: 'Orders aan het laden',
+    loading: true,
+    edit_price_id: null,
+    edit_price_changed: false
   },
+  computed: {
+    order_sum = function () {
+      var sum = 0.00;
+      this.orders.forEach(function (order) {
+        sum += order.price * order.n;
+      });
+      // Return result with two digits after the comma
+      return sum.toFixed(2).replace('.', ',');
+    }
+  },
+  methods: {
+    update_data = {
+      // If the user has changed something in the price, update it in the database
+      if (edit_price_changed) {
+        axios.post('/update_price').then(response => {
+          edit_price_changed = false;
+          edit_price_id = null;
+        });
+      }
+    }
+  }
   mounted() {
     refreshIntervalId = setInterval(function () {
       checkForTag();
@@ -18,8 +42,8 @@ function checkForTag () {
   axios.get('/random').then(response => {
     if (response.data === "Geen NFC tag") {
       // No tag found -> leave current active tag as is
-     // this.message = "Geen NFC tag"
-   } else if (vm.tag_id == response.data) {
+      // this.message = "Geen NFC tag"
+   } else if (vm.tag_id === response.data) {
       // Tag is same as before -> ignore
 
     } else {
@@ -28,15 +52,12 @@ function checkForTag () {
        vm.loadingtext = 'Loading...';
        loadOrders(vm.tag_id);
     }
-  }
-  );
+  });
 }
 
 function loadOrders(tag_id) {
   axios.get('/orders/' + tag_id).then(response => {
-      Vue.nextTick(function () {
-        vm.loadingtext = '';
-        vm.orders = response.data;
-      })
-    });
-  }
+    vm.loading = false;
+    vm.orders = response.data;
+  });
+}
